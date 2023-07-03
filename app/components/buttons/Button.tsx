be9cf5388536303929b547389aa81e68f3c5af10
cho0op <sorintema@gmail.com>
2023-07-03 13:54:48 +0300

    Added ButtonProps interface

--- app/components/buttons/Button.tsx
+++ app/components/buttons/Button.tsx
@@ -1,5 +1,17 @@
 'use client';
 
+import React from 'react';
+
+interface ButtonProps {
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
 const Button = () => {
   return (
     <div>

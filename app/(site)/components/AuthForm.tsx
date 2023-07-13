e0a709a90474c019d4629a7034230953ecdff87f
cho0op <sorintema@gmail.com>
2023-07-13 15:22:24 +0300

    Added google login 1:57:43

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -139,7 +139,7 @@ const AuthForm = () => {
           />
           <AuthSocialButton
             icon={BsGoogle}
-            onClick={() => socialAction('github')}
+            onClick={() => socialAction('google')}
           />
         </div>
       </div>

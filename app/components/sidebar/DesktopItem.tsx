a15c67c8971d8efe838e472a3f33c4a6e1ee8477
cho0op <sorintema@gmail.com>
2023-07-19 10:55:41 +0300

    Added routes to sidebar

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import clsx from 'clsx';
+import Link from 'next/link';
+
 interface DesktopItemProps {
   href: string;
   label: string;
@@ -10,12 +13,42 @@ interface DesktopItemProps {
 
 const DesktopItem: React.FC<DesktopItemProps> = ({
   active,
-  icon,
+  icon: Icon,
   onClick,
   label,
   href,
 }) => {
-  return <div>{label}</div>;
+  const handleClick = () => {
+    if (onClick) {
+      return onClick();
+    }
+  };
+  return (
+    <li onClick={handleClick}>
+      <Link
+        href={href}
+        className={clsx(
+          `
+        group
+        flex
+        gap-x-3
+        rounded-md
+        p-3
+        text-sm
+        font-semibold
+        leading-6
+        text-gray-600
+        hover:bg-gray-100
+        hover:text-black
+      `,
+          active && 'bg-gray-100 text-black'
+        )}
+      >
+        <Icon className='h-6 w-6 shrink-0' />
+        <span className='sr-only'>{label}</span>
+      </Link>
+    </li>
+  );
 };
 
 export default DesktopItem;

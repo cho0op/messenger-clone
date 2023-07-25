4760c3114adc349f09fd5813be928221418f95af
cho0op <sorintema@gmail.com>
2023-07-25 17:30:33 +0300

    Added users list 3:14:22

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,14 +1,57 @@
 'use client';
 
+import Avatar from '@/app/components/sidebar/Avatar';
 import { User } from '@prisma/client';
-import { ReactElement } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { ReactElement, useCallback, useState } from 'react';
 
 interface UserBoxProps {
   data: User;
 }
 
 const UserBox = ({ data }: UserBoxProps): ReactElement => {
-  return <div>фывфыв</div>;
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = useCallback(async () => {
+    setIsLoading(true);
+
+    axios
+      .post('/api/conversations/', {
+        userId: data.id,
+      })
+      .then((response) => {
+        router.push(`/conversations/${response.data.id}`);
+      });
+  }, [data.id, router]);
+
+  return (
+    <div
+      onClick={handleClick}
+      className='
+        transition-pointer
+        relative
+        flex
+        w-full
+        items-center
+        space-x-3
+        rounded-lg
+        bg-white
+        p-3
+        hover:bg-neutral-100
+      '
+    >
+      <Avatar user={data} />
+      <div className={'min-w-0 flex-1'}>
+        <div className='focus:outline-none'>
+          <div className='mb-1 flex items-center justify-between'>
+            <p className='text-sm font-medium text-gray-900'>{data.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserBox;
